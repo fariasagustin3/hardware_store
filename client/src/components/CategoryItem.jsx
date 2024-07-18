fac3b90ce5ecb95c649cc4dca086d8d99ba2f7b0
fariasagustin3 <fariasagustin3@gmail.com>
2024-07-18 10:35:13 -0300

    (client/pages/homepage): homepage created

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CategoryItem = ({ category }) => {
   return (
-    <div className='max-w-[200px] max-h-[200px] bg-white px-5 py-5 rounded-md shadow-lg overflow-hidden'>
+    <div className='max-w-[200px] max-h-[200px] bg-white px-5 py-5 rounded-md shadow-lg overflow-hidden cursor-pointer'>
       <div className='flex flex-col'>
         <img src={category.image} alt="" className='w-full h-full' />
         <h2 className='text-black font-medium text-lg text-center mt-5'>{category.name}</h2>
--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -3,20 +3,20 @@ import { Link } from "wouter";
 const TopBar = ({ brands }) => {
   return (
     <div className="w-screen h-full bg-black">
-      <div className="px-3 py-5 flex flex-col">
+      <div className="px-3 py-5 flex flex-col md:flex-row md:items-center md:justify-between">
         {/* brands */}
         <div className="flex items-center flex-wrap gap-5">
           {brands.map((brand) => (
             <div key={brand.id}>
               <Link href="/products">
-                <img src={brand.logo} className="w-14" alt="" />
+                <img src={brand.logo} className="w-14 md:w-20" alt="" />
               </Link>
             </div>
           ))}
         </div>
 
         {/* contact data */}
-        <div className="mt-7 flex items-center justify-center">
+        <div className="mt-7 flex items-center justify-center md:mt-0">
           <div className="flex items-center gap-2 border-r-[1px] border-r-[var(--gray)] px-3">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -24,7 +24,7 @@ const TopBar = ({ brands }) => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="#C7C7C7"
-              className="w-4 h-4"
+              className="w-4 h-4 md:w-6 md:w-6"
             >
               <path
                 strokeLinecap="round"
@@ -32,7 +32,7 @@ const TopBar = ({ brands }) => {
                 d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
               />
             </svg>
-            <span className="text-[var(--gray)] font-medium text-sm">
+            <span className="text-[var(--gray)] font-medium text-sm md:text-lg">
               1145236585
             </span>
           </div>
@@ -44,7 +44,7 @@ const TopBar = ({ brands }) => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="black"
-              className="w-5 h-5"
+              className="w-5 h-5 md:w-6 md:w-6"
             >
               <path
                 strokeLinecap="round"
@@ -53,7 +53,7 @@ const TopBar = ({ brands }) => {
               />
             </svg>
 
-            <span className="text-[var(--gray)] font-medium text-sm">
+            <span className="text-[var(--gray)] font-medium text-sm md:text-lg">
               contacto@gmail.com
             </span>
           </div>
